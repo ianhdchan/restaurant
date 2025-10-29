2092a7f1997762b7399e8ab4a82f857e53391976
Ian Chan <ianchan1108@hotmail.com>
2025-10-29 16:04:44 -0700

    add order path

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ import { Navbar } from "./components/Navbar";
 import { Locations } from "./pages/Locations";
 import { MenuProvider } from "./context/MenuContext";
 import { Footer } from "./components/Footer";
+import { Order } from "./pages/Order";
 
 function App() {
   return (
-      <MenuProvider>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/locations" element={<Locations />} />
-          </Routes>
-        </main>
-        <Footer />
-      </MenuProvider>
+    <MenuProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+      <Footer />
+    </MenuProvider>
   );
 }
 
