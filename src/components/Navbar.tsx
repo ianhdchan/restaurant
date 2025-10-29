8b2a7e9f7350ee73a0325141c49cb9ee54d0304f
Ian Chan <ianchan1108@hotmail.com>
2025-10-29 12:30:11 -0700

    add more information on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,21 +9,34 @@ import {
 
 export function Navbar() {
   return (
-    <header className="flex items-center justify-around py-2">
+    <header className="w-full sticky top-0 z-50 flex items-center justify-between px-6 py-3 bg-zinc-900/95 backdrop-blur shadow-md shadow-current">
       <div className="brand">
-        <Link to="/" className="brand-link">
-          Always BBQ
+        <Link
+          to="/"
+          className="flex flex-row items-center gap-3 brand-link text-white text-lg font-semibold"
+        >
+          <img
+            src="https://cdn.prod.website-files.com/68b1e253311fe5cc1a9525be/68b1e41e387563285c270651_logo.svg"
+            width="40"
+            alt="Logo"
+          />
+          DNA BBQ
         </Link>
       </div>
       <NavigationMenu>
-        <NavigationMenuList>
+        <NavigationMenuList className="flex gap-4">
           {/* Home */}
           <NavigationMenuItem>
             <NavigationMenuLink
               asChild
               className={navigationMenuTriggerStyle()}
             >
-              <Link to="/">Home</Link>
+              <Link
+                to="/"
+                className="text-white px-3 py-1 rounded bg-transparent"
+              >
+                <p className="hover:text-orange-400"> Home</p>
+              </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
 
@@ -33,22 +46,39 @@ export function Navbar() {
               asChild
               className={navigationMenuTriggerStyle()}
             >
-              <Link to="/menu">Menu</Link>
+              <Link
+                to="/menu"
+                className="text-white px-3 py-1 rounded bg-transparent hover:text-orange-400"
+              >
+                Menu
+              </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
 
           {/* Locations */}
-          {/* Menu */}
           <NavigationMenuItem>
             <NavigationMenuLink
               asChild
               className={navigationMenuTriggerStyle()}
             >
-              <Link to="/locations">Locations</Link>
+              <Link
+                to="/locations"
+                className="text-white px-3 py-1 rounded bg-transparent hover:text-orange-400"
+              >
+                Locations
+              </Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+      <div className="ml-4">
+        <Link
+          to="#"
+          className="inline-flex items-center rounded-md bg-orange-500 px-3 py-2 text-sm font-medium text-white hover:bg-orange-400 transition-colors"
+        >
+          Order Online
+        </Link>
+      </div>
     </header>
   );
 }
