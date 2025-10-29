248f69c7cdabdcdaa51cd4d3846f46dd3c4efb3f
Ian Chan <ianchan1108@hotmail.com>
2025-10-29 09:49:48 -0700

    create ui components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { Link, Links } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+
+export function Navbar() {
+  return (
+    <header className="flex items-center justify-around py-2">
+      <div className="brand">
+        <Link to="/" className="brand-link">
+          Always BBQ
+        </Link>
+      </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {/* Home */}
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link to="/">Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          {/* Menu */}
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link to="/menu">Menu</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          {/* Locations */}
+          {/* Menu */}
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link to="/locations">Locations</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+}
