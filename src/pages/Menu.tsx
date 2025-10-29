6d88c8d95b32a648b75674f185a9c52cdfc8b134
Ian Chan <ianchan1108@hotmail.com>
2025-10-29 16:07:17 -0700

    update menu for more spacing, and formatting

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -4,16 +4,19 @@ import type { Menu } from "@/types/menu";
 
 export function Menu() {
   const { menu } = useMenuContext();
+  const someItems = menu.filter((item) => item.rate > 4).slice(0, 20);
 
   return (
     <>
       <div className="mt-3">
         <div>
-          <h1>Menu</h1>
-          
+          <h1 className="text-5xl py-15 font-extrabold">MENU</h1>
         </div>
+        <section className="py-20 bg-linear-to-b from-black to-zinc-950 text-white">
+          <h3>Our delicious meat sourced from the best Canadian farmers.</h3>
+        </section>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 w-full box-border">
-          {menu.map((food: Menu) => (
+          {someItems.map((food: Menu) => (
             <MenuCard key={food.id} menu={food} />
           ))}
         </div>
